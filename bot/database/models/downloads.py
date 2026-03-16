f702e8a53084ab5dd01facd2bbea932e34ae7df5
chems-eddine24 <obitouchiwa109@gmail.com>
2026-03-16 23:10:54 +0100

    fix: renamed platfrom to platform column

--- bot/database/models/downloads.py
+++ bot/database/models/downloads.py
@@ -11,7 +11,7 @@ class Download(Base):
     __tablename__ = "downloads"
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, default=uuid4())
     telegram_id = Column(String, unique=True, nullable=False)
-    platfrom = Column(String, nullable=False)
+    platform = Column(String, nullable=False)
     media_type = Column(String, nullable=False)
     url = Column(String, nullable=False)
-    download_at = Column(datetime(timezone=True), nullable=False)
+    downloaded_at = Column(datetime(timezone=True), nullable=False)
